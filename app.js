b6d50c1fda56c6a1fc2834a421d38cead14f02fe
Robert <robert.gumeny528@gmail.com>
2021-01-25 11:25:14 -0700

    updated

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ let questionThreeScore = 0;
 let questionFourScore = 0;
 let questionFiveScore = 0;
 let questionSixScore = 0;
+let questionSevenScore = 0;
+let questionEightScore = 0;
+let questionNineScore = 0;
+let questionTenScore = 0;
+let questionElevenScore = 0;
+let questionTwelveScore = 0;
 let totalScore = 0;
+let adjustedScore = 0;
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -26,12 +33,35 @@ function scoreTotal() {
     .value;
   questionSixScore = document.querySelector('input[name="dryEyes6"]:checked')
     .value;
-  score =
+  questionSevenScore = document.querySelector('input[name="dryEyes7"]:checked')
+    .value;
+  questionEightScore = document.querySelector('input[name="dryEyes8"]:checked')
+    .value;
+  questionNineScore = document.querySelector('input[name="dryEyes9"]:checked')
+    .value;
+  questionTenScore = document.querySelector('input[name="dryEyes10"]:checked')
+    .value;
+  questionElevenScore = document.querySelector(
+    'input[name="dryEyes11"]:checked'
+  ).value;
+  questionTwelveScore = document.querySelector(
+    'input[name="dryEyes12"]:checked'
+  ).value;
+  totalScore =
     parseInt(questionOneScore) +
     parseInt(questionTwoScore) +
     parseInt(questionThreeScore) +
     parseInt(questionFourScore) +
     parseInt(questionFiveScore) +
-    parseInt(questionSixScore);
-  scoreDisplay.innerHTML = ` ${score}`;
+    parseInt(questionSixScore) +
+    parseInt(questionSevenScore) +
+    parseInt(questionEightScore) +
+    parseInt(questionNineScore) +
+    parseInt(questionTenScore) +
+    parseInt(questionElevenScore) +
+    parseInt(questionTwelveScore);
+
+  adjustedScore = (totalScore * (100 / 48)).toFixed(2);
+
+  scoreDisplay.innerHTML = ` ${adjustedScore}`;
 }
