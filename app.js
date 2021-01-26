16328605c526645ecba37dcd2858d290483d1887
Robert <robert.gumeny528@gmail.com>
2021-01-26 10:35:31 -0700

    adjusted score formula

--- app.js
+++ app.js
@@ -1,67 +1,38 @@
 const form = document.getElementById("form");
 const scoreDisplay = document.getElementById("scoreDisplay");
-let questionOneScore = 0;
-let questionTwoScore = 0;
-let questionThreeScore = 0;
-let questionFourScore = 0;
-let questionFiveScore = 0;
-let questionSixScore = 0;
-let questionSevenScore = 0;
-let questionEightScore = 0;
-let questionNineScore = 0;
-let questionTenScore = 0;
-let questionElevenScore = 0;
-let questionTwelveScore = 0;
+
+let scores = [];
+let nonZeroScores = [];
 let totalScore = 0;
 let adjustedScore = 0;
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  scoreTotal();
+  getAdjustedScore();
 });
 
-function scoreTotal() {
-  questionOneScore = document.querySelector('input[name="dryEyes1"]:checked')
-    .value;
-  questionTwoScore = document.querySelector('input[name="dryEyes2"]:checked')
-    .value;
-  questionThreeScore = document.querySelector('input[name="dryEyes3"]:checked')
-    .value;
-  questionFourScore = document.querySelector('input[name="dryEyes4"]:checked')
-    .value;
-  questionFiveScore = document.querySelector('input[name="dryEyes5"]:checked')
-    .value;
-  questionSixScore = document.querySelector('input[name="dryEyes6"]:checked')
-    .value;
-  questionSevenScore = document.querySelector('input[name="dryEyes7"]:checked')
-    .value;
-  questionEightScore = document.querySelector('input[name="dryEyes8"]:checked')
-    .value;
-  questionNineScore = document.querySelector('input[name="dryEyes9"]:checked')
-    .value;
-  questionTenScore = document.querySelector('input[name="dryEyes10"]:checked')
-    .value;
-  questionElevenScore = document.querySelector(
-    'input[name="dryEyes11"]:checked'
-  ).value;
-  questionTwelveScore = document.querySelector(
-    'input[name="dryEyes12"]:checked'
-  ).value;
-  totalScore =
-    parseInt(questionOneScore) +
-    parseInt(questionTwoScore) +
-    parseInt(questionThreeScore) +
-    parseInt(questionFourScore) +
-    parseInt(questionFiveScore) +
-    parseInt(questionSixScore) +
-    parseInt(questionSevenScore) +
-    parseInt(questionEightScore) +
-    parseInt(questionNineScore) +
-    parseInt(questionTenScore) +
-    parseInt(questionElevenScore) +
-    parseInt(questionTwelveScore);
+function buildAnswerArr() {
+  for (let i = 1; i <= 12; i++) {
+    let res = document.querySelector(`input[name="dryEyes${i}"]:checked`).value;
+    scores.push(parseInt(res));
+  }
+}
 
-  adjustedScore = Math.round(totalScore / 4);
+nonZeroScores = filterOutZeros(scores);
+totalScore = getTotalScore(scores);
 
-  scoreDisplay.innerHTML = ` ${adjustedScore}`;
+function filterOutZeros(scores) {
+  return scores.filter((score) => score > 0);
+}
+
+function getTotalScore(scores) {
+  return scores.reduce((a, b) => a + b, 0);
+}
+
+function getAdjustedScore() {
+  buildAnswerArr();
+  nonZeroScores = filterOutZeros(scores);
+  totalScore = getTotalScore(scores);
+  adjustedScore = (totalScore * 100) / (nonZeroScores.length * 4);
+  scoreDisplay.innerHTML = ` ${Math.round(adjustedScore)}`;
 }
